a243a1a2ce68e4bae14fb0e713b7b99c6042a56a
Iker Garcia <ikergarciadev@gmail.com>
2022-07-24 18:26:58 +0200

    feat(Home): :construction: add home component (v0.1) (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Home from '@/components/home/Home'
+import { createTheme, CssBaseline, Grid, ThemeProvider } from '@mui/material'
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000C24',
+    },
+    secondary: {
+      main: '#FBFBFB',
+    },
+    text: {
+      primary: '#000C24',
+      secondary: '#F44336',
+    },
+    common: {
+      white: '#FBFBFB',
+    },
+  },
+  typography: {
+    h1: {
+      fontWeight: 700,
+      fontSize: 32,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 600,
+    },
+    subtitle1: {
+      fontWeight: 600,
+    },
+    body1: {
+      fontSize: 24,
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 1024,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+})
+export default function App() {
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/assets/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src='assets/react.svg' className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid container>
+        <Home />
+      </Grid>
+    </ThemeProvider>
   )
 }
-
-export default App
