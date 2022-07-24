f624b17a16f0ade155c5c6a94b04b3a47ef0b442
Iker Garcia <ikergarciadev@gmail.com>
2022-07-24 15:40:45 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  plugins: [react(), svgrPlugin()],
+})
