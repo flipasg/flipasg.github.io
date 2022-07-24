2e36f35a56315b2b59a84488d45407092d13f533
Iker Garcia <ikergarciadev@gmail.com>
2022-07-24 17:05:01 +0200

    :bento: adding some statis assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
